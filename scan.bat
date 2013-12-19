d52bac695f521594ab1ea191bd3fe634636e4a04
Ian Knowles <ian@ciyam.com>
2013-12-19 17:35:36 +0800

    Removed 'importprivkey' and extended scan to use zbarcam or zbarimg.

--- scan.bat
+++ scan.bat
@@ -1,18 +1,30 @@
 @echo off
 REM Copyright (c) 2013 CIYAM Developers
 
+setlocal
+
 if "%1" == "" goto usage
 if exist "%1" goto error1
 
+set input_file=%cd%\%2
+
 pushd "C:\Program Files\ZBar\bin"
-zbarcam --raw -Sdisable -Sqrcode.enable>~send.txt
+
+if "%2" == "" goto use_cam
+zbarimg --raw -Sdisable -Sqrcode.enable %input_file%>~scan.txt
+goto next
+
+:use_cam
+zbarcam --raw -Sdisable -Sqrcode.enable>~scan.txt
+
+:next
 popd
-move "C:\Program Files\ZBar\bin\~send.txt" %1
+move "C:\Program Files\ZBar\bin\~scan.txt" %1
 type %1
 goto end
 
 :usage
-echo Usage: scan [output file name]
+echo Usage: scan [output file] [[input file]]
 goto end
 
 :error1
@@ -20,3 +32,5 @@ echo Error: File '%1' already exists.
 goto end
 
 :end
+endlocal
+
